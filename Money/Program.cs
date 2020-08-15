c554c80316575f9d37e674d52da88fc019137b96
Arun Stephens <a@arun.nz>
2020-08-15 21:30:05 +1200

    test the other kiwibank accounts

--- Money/Program.cs
+++ Money/Program.cs
@@ -1,5 +1,7 @@
-﻿using Money.Importers;
+﻿using Money.Data.Model;
+using Money.Importers;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Money
@@ -12,26 +14,25 @@ namespace Money
 
             IImporter importer = new CsvImporter<Importers.Model.BnzTransaction>(true, false);
 
-            await foreach (var tx in importer.Import(@"C:\Users\a\Documents\Money\Joint---Main-9AUG2018-to-9AUG2020.csv"))
-            {
-                Console.WriteLine(tx.TransactionDate + " " + tx.Amount + " " + tx.ExternalId);
-            }
+            await WriteTransactions(importer.Import(@"C:\Users\a\Documents\Money\Joint---Main-9AUG2018-to-9AUG2020.csv"));
 
             importer = new CsvImporter<Importers.Model.KiwibankCreditCardTransaction>(false, true);
 
-            await foreach (var tx in importer.Import(@"C:\Users\a\Documents\Money\4833-48 - -3016_10Aug.CSV"))
-            {
-                Console.WriteLine(tx.TransactionDate + " " + tx.Amount + " " + tx.ExternalId);
-            }
+            await WriteTransactions(importer.Import(@"C:\Users\a\Documents\Money\4833-48 - -3016_10Aug.CSV"));
 
             importer = new CsvImporter<Importers.Model.KiwibankBankTransaction>(true, false);
 
-            await foreach (var tx in importer.Import(@"C:\Users\a\Documents\Money\38-9018-0371564-02_10Aug.CSV"))
+            await WriteTransactions(importer.Import(@"C:\Users\a\Documents\Money\38-9018-0371564-02_10Aug.CSV"));
+            await WriteTransactions(importer.Import(@"C:\Users\a\Documents\Money\38-9018-0371564-01_15Aug.CSV"));
+            await WriteTransactions(importer.Import(@"C:\Users\a\Documents\Money\38-9018-0371564-03_15Aug.CSV"));
+        }
+
+        private static async Task WriteTransactions(IAsyncEnumerable<Transaction> transactions)
+        {
+            await foreach (var tx in transactions)
             {
                 Console.WriteLine(tx.TransactionDate + " " + tx.Amount + " " + tx.ExternalId);
             }
-
-
         }
     }
 }
