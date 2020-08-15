60dead5de85a482cd369c312981c10f7700331c3
Arun Stephens <a@arun.nz>
2020-08-16 11:17:32 +1200

    payee tables

---
+++ Money.Data/Migrations/20200815231621_Payee.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Money.Data.Migrations
+{
+    public partial class Payee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayeeAlternateName_Payee_PayeeId",
+                table: "PayeeAlternateName");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Payee_PayeeId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payee",
+                table: "Payee");
+
+            migrationBuilder.RenameTable(
+                name: "Payee",
+                newName: "Payees");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payees",
+                table: "Payees",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayeeAlternateName_Payees_PayeeId",
+                table: "PayeeAlternateName",
+                column: "PayeeId",
+                principalTable: "Payees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Payees_PayeeId",
+                table: "Transactions",
+                column: "PayeeId",
+                principalTable: "Payees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PayeeAlternateName_Payees_PayeeId",
+                table: "PayeeAlternateName");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Payees_PayeeId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Payees",
+                table: "Payees");
+
+            migrationBuilder.RenameTable(
+                name: "Payees",
+                newName: "Payee");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Payee",
+                table: "Payee",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PayeeAlternateName_Payee_PayeeId",
+                table: "PayeeAlternateName",
+                column: "PayeeId",
+                principalTable: "Payee",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Payee_PayeeId",
+                table: "Transactions",
+                column: "PayeeId",
+                principalTable: "Payee",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
